02b6e17f6b99054236a742b2c7c4807dc1aef1dd
Jorge Torres <jtorres1@ucol.mx>
2023-05-03 17:49:15 -0600

    implementacion del diseño de login

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Navbar from './components/Navbar'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import RoutesIndex from './routes/Index'
 function App () {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <RoutesIndex />
+    </>
   )
 }
 
